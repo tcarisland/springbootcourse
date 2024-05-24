2da5c5771acfb02966cdcbd67cd3ffa279140b6d
tcarisland <tcarisland@gmail.com>
2024-05-24 15:37:22 +0200

    stashing

---
+++ section 4 rest/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,31 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    // TODO refactor this later
+    // quick and dirty: inject employee dao
+    @Autowired
+    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+        this.employeeDAO = theEmployeeDAO;
+    }
+
+    // expose "/employees" and return a list of employees
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+}
